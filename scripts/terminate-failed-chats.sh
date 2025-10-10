c2c49020e59c3302121b27ddbbd97d77014930d2
ffrose <119657383+ffrose@users.noreply.github.com>
2025-10-10 16:58:08 +0300

    Update terminate-failed-chats.sh

--- scripts/terminate-failed-chats.sh
+++ scripts/terminate-failed-chats.sh
@@ -5,13 +5,14 @@ pwd
 echo $(date -u +"%Y-%m-%d %H:%M:%S.%3NZ") - $script_name started
 . constants.ini
 
-#get_new_nonce() {
-#  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
-#  nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
-#  echo "$nonce"
-#}
+get_new_nonce() {
+  response=$(curl -s -X POST -H "Content-Type: application/json" "$TRAINING_RESQL/get-new-nonce")
+  nonce=$(echo "$response" |grep -Eo "([a-f0-9-]+-){4}[a-f0-9-]+")
+  echo "$nonce"
+}
 
 dead_chat_ids=$(curl -s \
+  -H "x-ruuter-nonce: $(get_new_nonce)" \
   -H "Content-Type: application/json" \
   "http://component-byk-ruuter-private:8080/backoffice/cron-tasks/end-dead-chats")
 
